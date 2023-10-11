f249bbd52c8550ee37576b55698cdd7af0d35c82
ryosyntaxx <riodwi12174@gmail.com>
2023-10-11 09:43:21 +0700

    add gita json

--- src/main/java/com/jkt48/controller/Controllers.java
+++ src/main/java/com/jkt48/controller/Controllers.java
@@ -1,10 +1,16 @@
 package com.jkt48.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class Controllers {
+
+    @PostMapping
+    private void insertDB() {
+
+    }
 
 }
